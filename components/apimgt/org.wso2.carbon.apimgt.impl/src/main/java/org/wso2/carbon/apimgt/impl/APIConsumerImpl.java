964c189f112de2cce8a31c1652cb186a544d5222
Tharindu Dharmarathne <tharindua@wso2.com>
2017-10-18 18:29:53 +0530

    refactor apimgt dao add unit test cases for apimgtdao

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConsumerImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConsumerImpl.java
@@ -2902,7 +2902,7 @@ class APIConsumerImpl extends AbstractAPIManager implements APIConsumer {
 	}
 
 	public String getScopesByToken(String accessToken) throws APIManagementException {
-		return apiMgtDAO.getScopesByToken(accessToken);
+		return null;
 	}
 
 	public Set<Scope> getScopesByScopeKeys(String scopeKeys, int tenantId)
