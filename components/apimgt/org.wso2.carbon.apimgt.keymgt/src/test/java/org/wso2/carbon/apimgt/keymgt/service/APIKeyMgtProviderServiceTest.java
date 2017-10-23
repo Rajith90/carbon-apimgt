98c2d8f7f1cfc72de06b31a2e666c0c13659337b
Rukshan <ruky68@live.com>
2017-10-23 18:11:53 +0530

    added assert function

--- components/apimgt/org.wso2.carbon.apimgt.keymgt/src/test/java/org/wso2/carbon/apimgt/keymgt/service/APIKeyMgtProviderServiceTest.java
+++ components/apimgt/org.wso2.carbon.apimgt.keymgt/src/test/java/org/wso2/carbon/apimgt/keymgt/service/APIKeyMgtProviderServiceTest.java
@@ -75,18 +75,25 @@ public class APIKeyMgtProviderServiceTest {
     public void testActivateAccessTokens() throws Exception {
         APIKeyMgtProviderService apiKeyMgtProviderService = new APIKeyMgtProviderService();
         apiKeyMgtProviderService.activateAccessTokens(new String[] { USER_NAME }, new APIInfoDTO());
+        Mockito.verify(apiMgtDAO, Mockito.atLeastOnce())
+                .changeAccessTokenStatus(Mockito.anyString(), Mockito.any(APIInfoDTO.class), Mockito.anyString());
     }
 
     @Test
     public void testBlockAccessTokens() throws Exception {
         APIKeyMgtProviderService apiKeyMgtProviderService = new APIKeyMgtProviderService();
         apiKeyMgtProviderService.blockAccessTokens(new String[] { USER_NAME }, new APIInfoDTO());
+        Mockito.verify(apiMgtDAO, Mockito.atLeastOnce())
+                .changeAccessTokenStatus(Mockito.anyString(), Mockito.any(APIInfoDTO.class), Mockito.anyString());
     }
 
     @Test
     public void testRevokeAccessTokens() throws Exception {
         APIKeyMgtProviderService apiKeyMgtProviderService = new APIKeyMgtProviderService();
         apiKeyMgtProviderService.revokeAccessTokens(new String[] { USER_NAME }, new APIInfoDTO());
+        Mockito.verify(apiMgtDAO, Mockito.atLeastOnce())
+                .changeAccessTokenStatus(Mockito.anyString(), Mockito.any(APIInfoDTO.class), Mockito.anyString());
+
     }
 
     @Test
