99af1c42fd98682c18911a52ee6e18bc2d75df0b
npamudika <rpnpamudika@gmail.com>
2018-02-16 12:26:23 +0530

    Fix throwing NPE when invoking a web socket API with Analytics enabled

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/WebsocketInboundHandler.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/WebsocketInboundHandler.java
@@ -29,7 +29,6 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.http.HttpHeaders;
 import org.json.simple.JSONObject;
 import org.wso2.carbon.apimgt.api.APIManagementException;
-import org.wso2.carbon.apimgt.api.model.Application;
 import org.wso2.carbon.apimgt.gateway.handlers.security.APISecurityConstants;
 import org.wso2.carbon.apimgt.gateway.handlers.security.APISecurityException;
 import org.wso2.carbon.apimgt.gateway.handlers.security.APISecurityUtils;
@@ -37,7 +36,6 @@ import org.wso2.carbon.apimgt.gateway.handlers.throttling.APIThrottleConstants;
 import org.wso2.carbon.apimgt.gateway.throttling.publisher.ThrottleDataPublisher;
 import org.wso2.carbon.apimgt.gateway.utils.APIMgtGoogleAnalyticsUtils;
 import org.wso2.carbon.apimgt.impl.APIConstants;
-import org.wso2.carbon.apimgt.impl.dao.ApiMgtDAO;
 import org.wso2.carbon.apimgt.impl.dto.APIKeyValidationInfoDTO;
 import org.wso2.carbon.apimgt.impl.utils.APIUtil;
 import org.wso2.carbon.apimgt.usage.publisher.APIMgtUsageDataBridgeDataPublisher;
@@ -328,8 +326,7 @@ public class WebsocketInboundHandler extends ChannelInboundHandlerAdapter {
         String useragent = headers.get(HttpHeaders.USER_AGENT);
 
         try {
-            Application app = getApplicationById(infoDTO);
-            String appOwner = app.getSubscriber().getName();
+            String appOwner = infoDTO.getSubscriber();
             String keyType = infoDTO.getType();
             String correlationID = UUID.randomUUID().toString();
 
@@ -344,7 +341,6 @@ public class WebsocketInboundHandler extends ChannelInboundHandlerAdapter {
             requestPublisherDTO.setConsumerKey(infoDTO.getConsumerKey());
             //context will always be empty as this method will call only for WebSocketFrame and url is null
             requestPublisherDTO.setContext("");
-//            requestPublisherDTO.setContext(getContextFromUrl(uri));
             requestPublisherDTO.setContinuedOnThrottleOut(isThrottledOut);
             requestPublisherDTO.setHostName(DataPublisherUtil.getHostAddress());
             requestPublisherDTO.setMethod("-");
@@ -363,9 +359,5 @@ public class WebsocketInboundHandler extends ChannelInboundHandlerAdapter {
             // flow should not break if event publishing failed
             log.error("Cannot publish event. " + e.getMessage(), e);
         }
-    }
-
-    protected Application getApplicationById(APIKeyValidationInfoDTO infoDTO) throws APIManagementException {
-        return ApiMgtDAO.getInstance().getApplicationById(Integer.parseInt(infoDTO.getApplicationId()));
     }
 }
