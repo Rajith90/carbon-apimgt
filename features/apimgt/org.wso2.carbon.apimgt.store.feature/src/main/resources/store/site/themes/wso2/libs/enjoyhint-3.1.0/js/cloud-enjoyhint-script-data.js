8e546f49cc21c21fab4a0a9a965fa8bb91025c9e
msm1992 <madusha.sachini@yahoo.com>
2018-04-10 10:40:03 +0530

    Fix abrupt wlakthrough exit when api link is not correctly clicked

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store/site/themes/wso2/libs/enjoyhint-3.1.0/js/cloud-enjoyhint-script-data.js
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store/site/themes/wso2/libs/enjoyhint-3.1.0/js/cloud-enjoyhint-script-data.js
@@ -198,7 +198,7 @@ var api_store_select_one_api = [
     },
     {
         selector: '.page-content',
-        selectNextElement : '.white-wrapper',
+        selectNextElement : '.square-element',
         targetNext : 'INSIDE_PARENT',
         event: 'click',
         description: 'Shown here is an API that is published in  our API Store. Click this API to open it.',
